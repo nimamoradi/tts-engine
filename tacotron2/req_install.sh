d5f4247480ad5dcd15afb31a3798b9793ed6c291
ni_moradi <ni_moradi@yahoo.com>
2019-08-18 20:05:27 +0430

    Initial commit

---
+++ tacotron2/req_install.sh
@@ -0,0 +1,20 @@
+pip uninstall -y tensorboardX
+pip install tensorboardX
+pip uninstall -y limgaug 
+pip uninstall -y matplotlib
+pip install matplotlib==2.0.1
+pip install imgaug==0.2.6
+pip install -q torch
+pip install apex 
+pip uninstall -y protobuf
+pip install --no-binary=protobuf protobuf
+pip install matplotlib==3.0.0
+pip install tensorflow
+pip install numpy==1.16.4
+pip install inflect==0.2.5
+pip uninstall -y librosa
+pip install librosa==0.6.2
+pip install scipy==1.0.0
+pip install Unidecode==1.0.22
+pip install pillow
+pip install numpy
