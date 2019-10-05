029ed0721f8e7298402d7bd96fc42f61306806bd
Mersad Esalati <mersad00esalati@gmail.com>
2019-10-05 12:52:20 +0330

    wtf

--- tacotron2/runner.py
+++ tacotron2/runner.py
@@ -38,8 +38,27 @@ for k in waveglow.convinv:
     k.float()
 denoiser = Denoiser(waveglow)
 
-text = "Hello World ! Hello World !"
-sequence = np.array(text_to_sequence(text, ['english_cleaners']))[None, :]
+text = u"سلام مرصاد و نیما"
+sequence = np.array(text_to_sequence(text, ['persian_cleaners']))[None, :]
+sequence = torch.autograd.Variable(
+    torch.from_numpy(sequence)).cuda().long()
+
+mel_outputs, mel_outputs_postnet, _, alignments = model.inference(sequence)
+plot_data((mel_outputs.float().data.cpu().numpy()[0],
+           mel_outputs_postnet.float().data.cpu().numpy()[0],
+           alignments.float().data.cpu().numpy()[0].T))
+
+with torch.no_grad():
+    audio = waveglow.infer(mel_outputs_postnet, sigma=0.666)
+ipd.Audio(audio[0].data.cpu().numpy(), rate=hparams.sampling_rate)
+
+
+audio_denoised = denoiser(audio, strength=0.01)[:, 0]
+ipd.Audio(audio_denoised.cpu().numpy(), rate=hparams.sampling_rate) 
+
+
+text = "سلام مرصاد و نیما"
+sequence = np.array(text_to_sequence(text, ['persian_cleaners']))[None, :]
 sequence = torch.autograd.Variable(
     torch.from_numpy(sequence)).cuda().long()
 
