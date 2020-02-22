f1fc87be5a10642305c0c5026fb29a4b2e924594
ni_moradi <ni_moradi@yahoo.com>
2020-02-22 22:48:01 +0330

    diemension

--- datasets/preprocessor.py
+++ datasets/preprocessor.py
@@ -35,7 +35,7 @@ def build_from_path(hparams, input_dirs, mel_dir, linear_dir, wav_dir, n_jobs=12
 				parts = line.strip().split('|')
 				basename = parts[0]
 				wav_path = os.path.join(input_dir, 'wavs', '{}.wav'.format(basename))
-				text = parts[2]
+				text = parts[1]
 				futures.append(executor.submit(partial(_process_utterance, mel_dir, linear_dir, wav_dir, basename, wav_path, text, hparams)))
 				index += 1
 
