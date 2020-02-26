41fc65bfa6d92189b2dea7e7cec359d70439f628
nima <ni_moradi@yahoo.com>
2020-02-26 07:47:37 +0000

    Update tacotron2/text/symbols.py, tacotron2/hparams.py files

--- tacotron2/hparams.py
+++ tacotron2/hparams.py
@@ -80,8 +80,8 @@ def create_hparams(hparams_string=None, verbose=False):
         prenet_dim=256,
         max_decoder_steps=1000,
         gate_threshold=0.5,
-        p_attention_dropout=0.5,
-        p_decoder_dropout=0.5,
+        p_attention_dropout=0.3,
+        p_decoder_dropout=0.3,
 
         # Attention parameters
         attention_rnn_dim=1024,
@@ -103,7 +103,7 @@ def create_hparams(hparams_string=None, verbose=False):
         learning_rate=1e-3,
         weight_decay=1e-6,
         grad_clip_thresh=1.0,
-        batch_size=8,
+        batch_size=5,
         mask_padding=True  # set model's padded outputs to padded values
     )
 
