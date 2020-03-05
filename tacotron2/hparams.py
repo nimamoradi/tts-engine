c65e33ebb892eaff7224af395a2cc072357f926d
nima <ni_moradi@yahoo.com>
2020-03-05 23:00:21 +0000

    Update tacotron2/hparams.py

--- tacotron2/hparams.py
+++ tacotron2/hparams.py
@@ -25,9 +25,9 @@ def create_hparams(hparams_string=None, verbose=False):
         # Data Parameters             #
         ################################
         load_mel_from_disk=False,
-        training_files='filelists/quran_text_train_filelist.txt',
-        validation_files='filelists/quran_text_val_filelist.txt',
-        test_files='filelists/quran_text_test_filelist.txt',
+        training_files='filelists/train.csv',
+        validation_files='filelists/val.csv',
+        test_files='filelists/test.csv',
         text_cleaners=['persian_cleaners'],
 
         ################################
