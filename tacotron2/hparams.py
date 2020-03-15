2e5d6251e7069125958f1eb5efe585d728c5b229
nima <ni_moradi@yahoo.com>
2020-03-15 16:36:25 +0000

    Update hparams.py

--- tacotron2/hparams.py
+++ tacotron2/hparams.py
@@ -79,9 +79,9 @@ def create_hparams(hparams_string=None, verbose=False):
         decoder_rnn_dim=1024,
         prenet_dim=256,
         max_decoder_steps=1000,
-        gate_threshold=0.5,
-        p_attention_dropout=0.4,
-        p_decoder_dropout=0.4,
+        gate_threshold=0.66,
+        p_attention_dropout=0.1,
+        p_decoder_dropout=0.1,
 
         # Attention parameters
         attention_rnn_dim=1024,
