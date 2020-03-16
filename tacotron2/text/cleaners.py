6e4315065f0d81d3abed2ef3a9430fcfaeeb8d92
nima <ni_moradi@yahoo.com>
2020-03-16 18:43:13 +0000

    Update tacotron2/text/cleaners.py

--- tacotron2/text/cleaners.py
+++ tacotron2/text/cleaners.py
@@ -91,7 +91,6 @@ def english_cleaners(text):
 
 
 def persian_cleaners(text):
-  text = convert_to_ascii(text)
   text = collapse_whitespace(text)
   text = text.replace("\u200c"," ")
   text = text.replace("«"," ")
