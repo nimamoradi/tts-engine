a2353732693d3ab1b0b02346640f175210010472
nima <ni_moradi@yahoo.com>
2020-03-18 08:19:22 +0000

    Update hparams.py

--- tacotron2/hparams.py
+++ tacotron2/hparams.py
@@ -78,7 +78,7 @@ def create_hparams(hparams_string=None, verbose=False):
         n_frames_per_step=1,  # currently only 1 is supported
         decoder_rnn_dim=1024,
         prenet_dim=256,
-        max_decoder_steps=1100,
+        max_decoder_steps=2200,
         gate_threshold=0.33,
         p_attention_dropout=0.1,
         p_decoder_dropout=0.1,
@@ -100,7 +100,7 @@ def create_hparams(hparams_string=None, verbose=False):
         # Optimization Hyperparameters #
         ################################
         use_saved_learning_rate=False,
-        learning_rate=1.5e-3,
+        learning_rate=1e-3,
         weight_decay=1e-6,
         grad_clip_thresh=1.0,
         batch_size=56,
