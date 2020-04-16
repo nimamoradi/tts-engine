7ebf327c0e89e08e8497b6834f6082ac1df3accc
nima <ni_moradi@yahoo.com>
2020-04-16 09:32:41 +0000

    Update tacotron2/text/symbols.py

--- tacotron2/text/symbols.py
+++ tacotron2/text/symbols.py
@@ -10,7 +10,7 @@ _pad        = '_'
 _punctuation = '!\'(),.:;؟ '
 _special = '-'
 _eos        = '~'
-_letters = 'ءئابپتثجچحخدذرزژسشصضطظعغفقکگلمنوهیآ'
+_letters = 'abcdefghijklmnopqrstuvwxyz'
 
 # Prepend "@" to ARPAbet symbols to ensure uniqueness (some are the same as uppercase letters):
 _arpabet = ['@' + s for s in cmudict.valid_symbols]
