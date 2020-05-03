33785f1be2c0d00fc8400491f3880d9ebeb52593
nima <ni_moradi@yahoo.com>
2020-05-03 12:02:14 +0000

    Update tacotron2/hparams.py

--- tacotron2/hparams.py
+++ tacotron2/hparams.py
@@ -24,7 +24,7 @@ def create_hparams(hparams_string=None, verbose=False):
         ################################
         # Data Parameters             #
         ################################
-        load_mel_from_disk=True,
+        load_mel_from_disk=False,
         training_files='filelists/ljs_audio_text_train_filelist.txt',
         validation_files='filelists/ljs_audio_text_val_filelist.txt',
         test_files='filelists/ljs_audio_text_test_filelist.txt',
